a3ba8a0f0d3cb02dd500ac51b26dee50cb3e6d08
yuki1569 <yuki.simada@gmail.com>
2022-01-20 12:07:14 +0900

    エラー修正

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,45 +1,50 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
-import IconButton from '@material-ui/core/IconButton';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import MenuIcon from '@material-ui/icons/Menu';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
-import { useRouter } from 'next/router';
-import Box from '@material-ui/core/Box';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import MenuIcon from "@material-ui/icons/Menu";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import {
+  makeStyles,
+  useTheme,
+  Theme,
+  createStyles,
+} from "@material-ui/core/styles";
+import { useRouter } from "next/router";
+import Box from "@material-ui/core/Box";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import Button from "@material-ui/core/Button";
 
-const drawerWidth =180;
+const drawerWidth = 180;
 
 const useStyles = makeStyles((theme) =>
   createStyles({
     root: {
-      display: 'flex',
+      display: "flex",
       backgroundColor: theme.palette.primary.main,
-      minHeight:'none'
+      minHeight: "none",
     },
     drawer: {
-      [theme.breakpoints.up('xl')]: {
+      [theme.breakpoints.up("xl")]: {
         width: drawerWidth,
         flexShrink: 0,
       },
     },
     toolBar: {
-      height: '9vh'
+      height: "9vh",
     },
     menuButton: {
       marginRight: theme.spacing(2),
-      [theme.breakpoints.up('sm')]: {
-        display: 'none',
+      [theme.breakpoints.up("sm")]: {
+        display: "none",
       },
     },
     // necessary for content to be below app bar
@@ -51,7 +56,7 @@ const useStyles = makeStyles((theme) =>
       flexGrow: 1,
       padding: theme.spacing(3),
     },
-  }),
+  })
 );
 
 // interface Props {
@@ -64,51 +69,51 @@ const useStyles = makeStyles((theme) =>
 
 const MENU_LIST = [
   {
-    title: 'Home',
+    title: "Home",
     // icon: <InboxIcon />,
-    href: '/',
+    href: "/",
   },
   {
-    title: '使い方',
+    title: "使い方",
     // icon: <InboxIcon />,
-    href: '/csr',
+    href: "/csr",
   },
   {
-    title: '設定・規約',
+    title: "設定・規約",
     // icon: <InboxIcon />,
-    href: '/csr/a',
+    href: "/csr/a",
   },
   {
-    title: 'よくある質問',
+    title: "よくある質問",
     // icon: <InboxIcon />,
-    href: '/isg',
-  }
+    href: "/isg",
+  },
 ];
 const pages = [
   {
-    title: 'Home',
+    title: "Home",
     // icon: <InboxIcon />,
-    href: '/',
+    href: "/",
   },
   {
-    title: '使い方',
+    title: "使い方",
     // icon: <InboxIcon />,
-    href: '/csr',
+    href: "/csr",
   },
   {
-    title: '設定・規約',
+    title: "設定・規約",
     // icon: <InboxIcon />,
-    href: '/csr/a',
+    href: "/csr/a",
   },
   {
-    title: 'よくある質問',
+    title: "よくある質問",
     // icon: <InboxIcon />,
-    href: '/isg',
-  }
+    href: "/isg",
+  },
 ];
 
 // const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 export default function NavBar(props) {
   const { window } = props;
@@ -121,7 +126,7 @@ export default function NavBar(props) {
   };
 
   // 追加
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
@@ -159,11 +164,12 @@ export default function NavBar(props) {
   //         </div>
   //       ))}
   //     </List>
-  
+
   //   </div>
   // );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
     <div className={classes.root}>
@@ -189,7 +195,7 @@ export default function NavBar(props) {
       </nav> */}
 
       {/* モバイル以下なら隠す -- モバイル画面以外で表示 */}
-          {/* <Hidden xsDown implementation="css">
+      {/* <Hidden xsDown implementation="css">
             <Drawer
               classes={{
                 paper: classes.drawerPaper,
@@ -216,69 +222,66 @@ export default function NavBar(props) {
           </Typography>
         </Toolbar>
       </AppBar> */}
-      
+
       {/* 追加 */}
       <Toolbar className={classes.toolBar}>
-        <Typography
+        {/* <Typography
             variant="h6"
             noWrap
             component="div"
             sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
           >
-      </Typography>
-      
-      <Box  sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            
-            <IconButton
-              size="medium"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page.title} onClick={handleCloseNavMenu}>
-                  <Typography >{page.title}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-      </Box>
-      
-      <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }} >
-            {pages.map((page) => (
-              <Button
-                key={page.title}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'flex' }}
-              >
-                {page.title}
-              </Button>
-            ))}
-      </Box>
-      </Toolbar>
+      </Typography> */}
 
-      
+        <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+          <IconButton
+            size="medium"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{
+              display: { xs: "block", md: "none" },
+            }}
+          >
+            {pages.map((page) => (
+              <MenuItem key={page.title} onClick={handleCloseNavMenu}>
+                <Typography>{page.title}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+
+        <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+          {pages.map((page) => (
+            <Button
+              key={page.title}
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: "white", display: "flex" }}
+            >
+              {page.title}
+            </Button>
+          ))}
+        </Box>
+      </Toolbar>
     </div>
   );
 }
